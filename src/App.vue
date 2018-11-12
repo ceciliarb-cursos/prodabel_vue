1f943200808bf88632865cec6d0b90ef08869f2d
unknown <Administrador@pc991007758900.pbh.rmi>
2018-11-12 17:00:28 -0200

    componente combobox

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Combobox name='teste'></Combobox>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Combobox from './components/Combobox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Combobox
+  },
+  data() {
+    return {
+      exibePrimeiro: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.exibePrimeiro = !this.exibePrimeiro;
+    }
   }
 }
 </script>
---
+++ src/components/Combobox.vue
@@ -0,0 +1,23 @@
+<template>
+    <select v-bind:name="name" v-bind:id="name">
+        <option value=""></option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: ['name', 'dados'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
